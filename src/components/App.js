6ca73c82acd01840a7592dbb0fd7bb7eec6367f1
Jeff Palmer <jeff@palmytree.com>
2020-07-07 18:00:54 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UnderConstruction from './UnderConstruction/UnderConstruction'
+import '../style/reset.css'
+import '../style/App.scss'
+
+const App = () => {
+	return (
+		<div className='App'>
+			<UnderConstruction />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import EmailIcon from '@material-ui/icons/Email';
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+
+const UnderConstruction = () => {
+	return (
+		<div className='UnderConstruction'>
+			<h1>Jeff Palmer Development</h1>
+			<div>
+				<IconButton
+					href='https://www.linkedin.com/in/jeffpalmdev/'
+					target='_blank'
+				>
+					<LinkedInIcon fontSize='large' />
+				</IconButton>
+				<IconButton href='https://github.com/jeffpalm' target='_blank'>
+					<GitHubIcon fontSize='large' />
+				</IconButton>
+				<IconButton href='mailto:jeff@jeffpalm.dev' target='_blank'>
+					<EmailIcon fontSize='large' />
+				</IconButton>
+			</div>
+		</div>
+	)
+}
+
+export default UnderConstruction
