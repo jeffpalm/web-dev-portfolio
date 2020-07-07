b191aefbf8fcad9cef24efde21ecedf92fc602ea
Jeff Palmer <jeff@palmytree.com>
2020-07-07 18:20:31 -0500

    finished under construction

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
+require('dotenv').config()
 const express = require('express'),
-  app = express()
+  app = express(),
+  {SERVER_PORT} = process.env
+
+app.use(express.static(__dirname + '/../build'))
+
+app.use(express.json())
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Servin and observin port: ${SERVER_PORT}`)
+})
