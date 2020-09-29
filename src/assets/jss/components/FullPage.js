11f5763728f19d72d6040404fd5e64e1f17b526f
Jeff Palmer <jeff@palmytree.com>
2020-09-29 11:56:15 -0500

    fixed vertical height of pages on mobile

--- src/assets/jss/components/FullPage.js
+++ src/assets/jss/components/FullPage.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
     root: {
-        height: '100vh',
+        minHeight: '100vh',
         width: '100%',
         margin: 0,
         backgroundColor: theme.palette.background.default,
